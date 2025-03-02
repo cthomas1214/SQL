1284f5bc52aaf40ef74a2a3902875592e6cf63a5
cthomas1214 <cthomas1214@outlook.com>
2025-03-02 16:28:05 -0500

    Add files via upload

---
+++ AAP_Most_Selling_Holiday_Products.sql
@@ -0,0 +1,46 @@
+WITH OptionsCombined (ProdOptions, ProdName, ProdId, ProdQuantity, NumberOfOptionValues) AS (
+
+SELECT 
+	STRING_AGG(po.ProductOptionValue, ', '), ohp.ProductName, ohp.ProductId, AVG(ProductQuantity), COUNT(po.ProductOptionValue)
+FROM 
+OrderHistory oh
+    
+JOIN 
+    OrderHistoryProducts ohp ON oh.OrderHistoryId = ohp.OrderHistoryId
+LEFT JOIN 
+    OrderHistoryProductOptions po ON ohp.OrderHistoryProductId = po.OrderHistoryProductId 
+
+WHERE 
+    oh.Pending = 0 
+	
+GROUP BY  ohp.ProductName, ohp.ProductId, ohp.OrderHistoryProductId, oh.OrderTotal)
+,
+AccurateQuantities (ProdOpQty, SumPrdQty, ProdNameQty, ProdIdQty) AS (
+SELECT ProdOptions, (SUM(ProdQuantity)) AS OptionNumberofOrdered, ProdName, ProdId
+FROM OptionsCombined
+GROUP BY ProdOptions, NumberOfOptionValues, ProdName, ProdId
+)
+,
+TotalQTYFullProds (FProdName, FProductId, SumProductQuantity) AS (
+SELECT op.ProductName, op.ProductId, SUM(op.ProductQuantity) AS TotalQuantity
+FROM OrderHistoryProducts op
+JOIN OrderHistory oh
+ON op.OrderHistoryId = oh.OrderHistoryId
+
+WHERE oh.Pending = 0
+
+GROUP BY op.ProductName, op.ProductId
+)
+
+SELECT TOP 100 ac.ProdOpQty, ac.SumPrdQty, ac.ProdNameQty, tqp.SumProductQuantity AS TotalQuantity, ac.ProdIdQty AS ProductId
+FROM AccurateQuantities ac
+JOIN TotalQTYFullProds tqp
+ON ac.ProdIdQty = tqp.FProductId
+JOIN ProductCategories pc
+ON ac.ProdIdQty = pc.ProductId
+JOIN Products P
+ON ac.ProdIdQty = P.ProductId
+
+WHERE pc.CategoryId = 32 AND P.IsDisabled = 0 AND P.ProductId > 964
+
+ORDER BY tqp.SumProductQuantity DESC, ac.ProdNameQty ASC, ac.SumPrdQty DESC
